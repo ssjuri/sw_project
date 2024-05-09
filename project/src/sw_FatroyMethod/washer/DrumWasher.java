7fb002189592a6cf7f685baa3d6941bc0ea9445b
juri <sockjr@naver.com>
2024-05-09 17:37:04 +0900

    Initial commit

---
+++ project/src/sw_FatroyMethod/washer/DrumWasher.java
@@ -0,0 +1,8 @@
+package sw_FatroyMethod.washer;
+
+public class DrumWasher implements Washer{
+    @Override
+    public void wash() {
+        System.out.println("드럼 세탁기가 작동합니다.");
+    }
+}
---
+++ project/src/sw_FatroyMethod/washer/Washer.java
@@ -0,0 +1,7 @@
+package sw_FatroyMethod.washer;
+
+public interface Washer {
+    // 세탁기를 나타내는 인터페이스
+        void wash();
+
+}
